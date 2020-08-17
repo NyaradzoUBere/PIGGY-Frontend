1d50a3b3d75804d712d5f9869478eb273d7852f3
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-17 10:47:09 -0600

    Add create user page + fetch

---
+++ create_account.js
@@ -0,0 +1,27 @@
+const baseURL = "http://localhost:3000"
+const loginURL = `${baseURL}/login`
+const usersURL = `${baseURL}/users`
+const createUserForm = document.getElementById("create-user")
+
+createUserForm.addEventListener("submit", createAccount)
+
+function createAccount(event) {
+    event.preventDefault()
+    const createUserFormData = new FormData(event.target)
+    const username = createUserFormData.get("username")
+    const password = createUserFormData.get("password")
+    
+    fetch(usersURL, {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+            })
+        })
+    
+}
+// createAccount()
